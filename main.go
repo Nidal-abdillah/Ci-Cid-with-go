bbc1c1800016761ce4a1e75afd69e8826eaa82c3
Nidal abdillah <Nidalallabdillah@gmail.com>
2024-04-23 16:43:38 +0700

    ini testing 4

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    msg := sayHello("Alice")
-    fmt.Println(msg)
+	msg := sayHello("Alice")
+	fmt.Println(msg)
 }
 
 func sayHello(name string) string {
-    return fmt.Sprintf("Hi %s", name)
+	// Change this to "Hello %s" instead of "Hi %s".
+	return fmt.Sprintf("Hello %s", name)
 }
